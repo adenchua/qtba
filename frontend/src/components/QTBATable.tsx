41995616ab6dc9e58f7adcd961f0dd8133df0a1f
Aden <adenchuayouliang@gmail.com>
2021-07-01 11:22:18 +0800

    add table

---
+++ frontend/src/components/QTBATable.tsx
@@ -0,0 +1,156 @@
+import React, { useState, useCallback } from "react";
+import CheckCircleIcon from "@material-ui/icons/CheckCircleOutline";
+import CheckedCircleIcon from "@material-ui/icons/CheckCircle";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "inline-block",
+    minWidth: "100%",
+    border: "1px solid #dbe3e8",
+    borderRadius: 4,
+  },
+  table: {
+    width: "100%",
+    borderCollapse: "collapse",
+  },
+  tableHeader: {
+    background: "#F6F6F6",
+  },
+  tableHeaderRow: {
+    padding: "8px",
+    borderBottom: "2px solid #E8E8E8",
+  },
+  tableHeaderText: {
+    textTransform: "uppercase",
+    color: "#A0A0A0",
+    fontWeight: "bold",
+  },
+  tableBodyRow: {
+    background: "#FFF",
+    "&:not(:last-child)": {
+      borderBottom: "1px solid #E8E8E8",
+    },
+  },
+  tableBodyRowColumn: {
+    padding: "8px",
+  },
+}));
+
+const QUESTIONS_BANK = [
+  {
+    question: "How do we find out more about you?",
+    questionIndex: 1,
+    likes: 46,
+    isStrikeThrough: true,
+  },
+  {
+    question:
+      "How can a customer service team recognize these customer needs and effectively solve them on a day-to-day basis?",
+    questionIndex: 2,
+    likes: 27,
+    isStrikeThrough: false,
+  },
+];
+
+const QTBATable = (): JSX.Element => {
+  const classes = useStyles();
+  const [votedQuestionIndexes, setVotedQuestionsIndexes] = useState<number[]>([]);
+
+  const handleVoteQuestion = (questionIndex: number): void => {
+    if (votedQuestionIndexes.includes(questionIndex)) {
+      return; // already in vote
+    }
+
+    setVotedQuestionsIndexes((prevState) => [...prevState, questionIndex]);
+  };
+
+  const handleUnvoteQuestion = (questionIndex: number): void => {
+    if (!votedQuestionIndexes.includes(questionIndex)) {
+      return;
+    }
+
+    const updatedQuestionIndexes = votedQuestionIndexes.filter(
+      (questionIndexInArray) => questionIndexInArray !== questionIndex
+    );
+    setVotedQuestionsIndexes(updatedQuestionIndexes);
+  };
+
+  const renderTableHeader = useCallback((): JSX.Element => {
+    return (
+      <thead>
+        <tr className={classes.tableHeader}>
+          <th className={classes.tableHeaderRow}>
+            <Typography align='left' className={classes.tableHeaderText} variant='body2'>
+              #
+            </Typography>
+          </th>
+          <th className={classes.tableHeaderRow}>
+            <Typography align='left' className={classes.tableHeaderText} variant='body2'>
+              Questions
+            </Typography>
+          </th>
+          <th className={classes.tableHeaderRow}>
+            <Typography align='left' className={classes.tableHeaderText} variant='body2'>
+              Vote
+            </Typography>
+          </th>
+          <th className={classes.tableHeaderRow}></th>
+        </tr>
+      </thead>
+    );
+  }, [classes.tableHeader, classes.tableHeaderRow, classes.tableHeaderText]);
+
+  const renderTableRow = (question: string, questionIndex: number): JSX.Element => {
+    return (
+      <tr className={classes.tableBodyRow} key={questionIndex}>
+        <td className={classes.tableBodyRowColumn}>
+          <Typography variant='body2' color='textSecondary'>
+            {questionIndex}
+          </Typography>
+        </td>
+        <td className={classes.tableBodyRowColumn}>
+          <Typography variant='body2' color='textSecondary'>
+            {question}
+          </Typography>
+        </td>
+        <td className={classes.tableBodyRowColumn}>
+          {!votedQuestionIndexes.includes(questionIndex) && (
+            <IconButton size='small' onClick={() => handleVoteQuestion(questionIndex)}>
+              <CheckCircleIcon fontSize='small' color='disabled' />
+            </IconButton>
+          )}
+          {votedQuestionIndexes.includes(questionIndex) && (
+            <IconButton size='small' onClick={() => handleUnvoteQuestion(questionIndex)}>
+              <CheckedCircleIcon fontSize='small' color='primary' />
+            </IconButton>
+          )}
+        </td>
+        <td className={classes.tableBodyRowColumn}>
+          <IconButton size='small'>
+            <MoreHorizIcon fontSize='small' color='disabled' />
+          </IconButton>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className={classes.root}>
+      <table className={classes.table} cellSpacing={0} cellPadding={0}>
+        {renderTableHeader()}
+        <tbody>
+          {QUESTIONS_BANK.map((qtba) => {
+            const { question, questionIndex } = qtba;
+            return renderTableRow(question, questionIndex);
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default QTBATable;
--- frontend/src/pages/CapabilityPage.tsx
+++ frontend/src/pages/CapabilityPage.tsx
@@ -1,21 +1,26 @@
 import Toolbar from "@material-ui/core/Toolbar";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import QTBATable from "../components/QTBATable";
 import { DRAWER_WIDTH } from "../utils/constants";
 
 const CapabilityPage = (): JSX.Element => {
   return (
-    <div>
+    <>
       <Navbar />
       <Sidebar />
       <Toolbar />
       <Container style={{ marginLeft: DRAWER_WIDTH }}>
-        <Typography variant='h4'>Questions to Be Answered (QTBA)</Typography>
+        <Typography variant='h4'>Questions to Be Answered</Typography>
+        <Box mt={4}>
+          <QTBATable />
+        </Box>
       </Container>
-    </div>
+    </>
   );
 };
 
